af3a95d3be5bcbe4a5abc07aecf039e78761f27a
franciscoabib <franciscoabib@gmail.com>
2023-10-25 02:13:34 -0300

    Se agrego Id en la ruta para borrar un libro por id, quedo así '/libros/Id/:id' todo esto en el archivo routes.js

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ export const router = Router(); // Crea una nueva instancia de Router y la asign
 
 router.get('/libros', libro.getAll); // Ruta HTTP GET, que llama al método getAll en el objeto libro, para obtener todos los libros.
 router.post('/libros', libro.add); // Ruta HTTP POST, que llama al método add en el objeto libro, para agregar un libro.
-router.delete('/libros/:id', libro.deleteId); // Ruta HTTP DELETE, que llama al método deleteId en el objeto libro, para eliminar un libro a través de su id.
+router.delete('/libros/Id/:id', libro.deleteId); // Ruta HTTP DELETE, que llama al método deleteId en el objeto libro, para eliminar un libro a través de su id.
 router.delete('/libros/ISBN/:ISBN', libro.deleteISBN); // Ruta HTTP DELETE, que llama al método deleteISBN en el objeto libro, para eliminar un libro a través de su ISBN.
 router.put('/libros/actualizar/:id', libro.update); // Ruta HTTP PUT, que llama al método update en el objeto libro, para actualizar un libro.
 router.get('/libros/:id', libro.getOne); // Ruta HTTP GET, que llama al método getOne en el objeto libro, para obtener un libro a través su ID.
